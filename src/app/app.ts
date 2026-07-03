097dc2a178c583a0a65cb401a49d960950dce823
Misael13d <misaelantonymerma@gmail.com>
2026-07-03 11:22:32 -0400

    servidor

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,5 +1,7 @@
-import { Component, signal } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { CommentsService } from './servicios/comments.service';
+import { Comments } from './modelos/comments.interface';
 
 @Component({
   selector: 'app-root',
@@ -9,4 +11,18 @@ import { RouterOutlet } from '@angular/router';
 })
 export class App {
   protected readonly title = signal('guia26');
+  private readonly xapiService = inject(CommentsService) 
+  protected listaComentarios = signal<Comments[]>([])
+  
+  ngOnInit():void{
+    this.xapiService.getComentarios().subscribe({
+      next: (dat) => {
+        this.listaComentarios.set(dat)
+        console.log('Datos recibidos de la API',dat)
+      },
+      error:(err) => {
+        console.log('Error al conectar con la API',err)
+      }
+    })
+  }
 }
